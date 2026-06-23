ac2510ba87d4e798ed03e15a03cad834c7731803
eyecandybsky-design <eyecandybsky@gmail.com>
2026-06-23 20:33:57 +0200

    Update boost_reposter.py

--- boost_reposter.py
+++ boost_reposter.py
@@ -5,7 +5,7 @@ from datetime import datetime, timezone, timedelta
 from atproto import Client
 
 TARGET_ACCOUNT = "big-dominio.bsky.social"
-OWN_ACCOUNT = "Hotbleusky.bsky.social"
+OWN_ACCOUNT = "eyescandy.bsky.social"
 
 RANDOM_POSTS = 1
 NEWEST_POSTS = 10
