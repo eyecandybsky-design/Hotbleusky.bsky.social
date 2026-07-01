db9a7742f322ec3c9d50f80cbfe4af8db6936353
eyecandybsky-design <eyecandybsky@gmail.com>
2026-07-01 23:34:36 +0200

    Update boost_reposter.py

--- boost_reposter.py
+++ boost_reposter.py
@@ -7,8 +7,8 @@ from atproto import Client
 TARGET_ACCOUNT = "big-dominio.bsky.social"
 OWN_ACCOUNT = "hotbleusky.bsky.social"
 
-RANDOM_POSTS = 1
-NEWEST_POSTS = 10
+RANDOM_POSTS = 0
+NEWEST_POSTS = 3
 OWN_POSTS = 3
 
 LOOKBACK_DAYS = 60
